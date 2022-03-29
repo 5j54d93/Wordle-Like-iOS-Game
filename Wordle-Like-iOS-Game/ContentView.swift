13bcb81092e31a477e8453458729fe2280ee8bdd
Ricky Chuang <songsongyyohya@gmail.com>
2022-03-29 23:31:44 +0800

    Initial Commit

---
+++ Wordle-Like-iOS-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wordle-Like-iOS-Game
+//
+//  Created by 莊智凱 on 2022/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wordle-Like-iOS-Game/Wordle_Like_iOS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_Like_iOS_GameApp.swift
+//  Wordle-Like-iOS-Game
+//
+//  Created by 莊智凱 on 2022/3/29.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_Like_iOS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
