4496a40c1e295f972ce582bb26aa8a492800c0eb
Ricky Chuang <songsongyyohya@gmail.com>
2022-04-03 15:06:25 +0800

    Create Wordle_Like_iOS_GameTests.swift

---
+++ Tests/Wordle-Like-iOS-GameTests/Wordle_Like_iOS_GameTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Wordle_Like_iOS_Game
+
+final class Wordle_Like_iOS_GameTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Wordle_Like_iOS_Game().text, "Hello, World!")
+    }
+}
